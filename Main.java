9e4139a12627a35e7835922eb4e0d23a004c5e7d
talok7799-dot <talok7799@gmail.com>
2026-03-17 20:06:02 +0530

    JAVA

--- Main.java
+++ Main.java
@@ -1,6 +1,16 @@
-public class Main {
-    public static void main(String[] args){
-        System.out.println("JAVA");
-    }
+class Main {
+    public static void main(String[] args) {
+        int a=1;
+        int b=2;
+        int c=100;
+        int diff=a-c;
+        int sum=a+b;
+        int mult=a*b*c;
+        int div=b/c;
+        System.out.println("Addition of a and b: " + sum);
+        System.out.println("Subtraction of a and c: " + diff);
+        System.out.println("Multiplication of a, b, and c: " + mult);
+        System.out.println("Division of b and c: " + div);
 
+    }
 }
---
+++ Main1.java
@@ -0,0 +1,17 @@
+class Main1 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        System.out.println("Unary Operator" + (a++));
+        System.out.println("Unary Operator" + (++b));
+        System.out.println("Binary Operator");
+        System.out.println("1+2" + 1 + 2);
+        System.out.println("1+2" + (1 + 2));
+        System.out.println(1 + 2 + "-3");
+        int increment = ++a * b++;
+        System.out.println(increment);
+        System.out.println("Ternary Operator");
+        int largestNumber = (a > b) ? a : b;
+        System.out.println("Largest of 2 numbers: " + largestNumber);
+    }
+}
